a62918b06c11281590cf0b3f3edcf935e78eef7d
Dickens92 <philipodickens92@gmail.com>
2023-07-26 14:06:19 -0700

    updates shell.h and shell.c

--- shell.c
+++ shell.c
@@ -1,48 +1,73 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - a shell program
- * @argc: arguments counts
- * @argv: List of arguments passed
+ * free_data - frees data structure
  *
- * Return: 0
+ * @datash: data structure
+ * Return: no return
  */
-int main(int argc, char **argv)
+void free_data(data_shell *datash)
 {
-	char *str, *nocomments, *trimmed;
-	int mode = 1;
-	shell_var_t var = {0, 0, 0};
+	unsigned int i;
 
-	var.pid = getpid();
-	/*signal(SIGINT, sigintHandler);*/
-	while (mode)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		str = NULL;
-		if (argc > 1)
-		{
-			if (fileProcessor(argv[1], &str) == 0)
-				exit(var.code);
-		}
-		else
-		{
-			if ((int)prompt(&str, &mode) == -1)
-			{
-				free(str);
-				exit(var.code);
-			}
-		}
-
-		trimmed = _trim(str);
-		nocomments = removeComment(trimmed);
-		free(str);
-		free(trimmed);
-		str = nocomments;
-		if (processLogical(argv[0], str, &var) == 1)
-			continue;
-		else if (processCmdSp(argv[0], str, &var) == 1)
-			continue;
-		else
-			processcmd(argv[0], str, &var);
+		free(datash->_environ[i]);
 	}
-	return (var.code);
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
